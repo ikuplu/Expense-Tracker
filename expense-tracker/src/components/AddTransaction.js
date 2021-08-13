631a378b2926f8538ae2ce66f4af5e92b389652e
Ismail Kuplu <i.kuplu@outlook.com>
2021-08-13 10:33:14 +0200

    Added useState hooks for handling expense type and amount inside AddTransaction component

--- expense-tracker/src/components/AddTransaction.js
+++ expense-tracker/src/components/AddTransaction.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+
+const [text, setText] = useState('');
+const [amount, setAmount] = useState(0);
 
 export const AddTransaction = () => {
   return (
     <>
       <h3>Add new transaction</h3>
-      <form >
+      <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
         </div>
         <div className="form-control">
-          <label htmlFor="amount"
-            >Amount <br />
-            (negative - expense, positive - income)</label
-          >
-          <input type="number" placeholder="Enter amount..." />
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
     </>
-  )
-}
+  );
+};
