d09c466beefe16aeb8a951ea4114efa274bcb2d8
Ismail Kuplu <i.kuplu@outlook.com>
2021-08-13 20:23:21 +0200

    Added a variable called 'sign' for letting know whether the amount is an expense or an income

--- expense-tracker/src/components/Transaction.js
+++ expense-tracker/src/components/Transaction.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 export const Transaction = ({ transaction }) => {
+  // Demonstrate the amount with a positive or negative sign in front
+  const sign = transaction.amount < 0 ? '-' : '+';
+
   return (
     <li className="minus">
-      {transaction.text} <span>-$400</span>
+      {transaction.text}{' '}
+      <span>
+        (// Use Math.abs() to avoid double negative signs by the amount)
+        {sign}${Math.abs(transaction.amount)}
+      </span>
       <button className="delete-btn">x</button>
     </li>
   );
